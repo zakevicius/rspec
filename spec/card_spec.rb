987e1776f183a54cf4f688fac9b40a209b5ac85c
zakevicius <m.zakevicius@gmail.com>
2022-10-31 15:19:39 +0200

    Describe, it

---
+++ spec/card_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Card
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+
+RSpec.describe Card do
+  it 'has a rank' do
+    card= Card.new('Ace', 'Spades')
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    card= Card.new('Ace', 'Spades')
+    expect(card.suit).to eq('Spades')
+  end
+end
---
+++ spec/school_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class School
+  attr_reader :name, :students
+
+  def initialize(name, students = [])
+    @name = name
+    @students = students
+  end
+end
+
+RSpec.describe School do
+  it 'has a name' do
+    school = School.new('Beverly Hills High School')
+    expect(school.name).to eq('Beverly Hills High School')
+  end
+
+  it 'should start off with no students' do
+    school = School.new('Notre Dame High School')
+    expect(school.students).to eq([])
+  end
+end
