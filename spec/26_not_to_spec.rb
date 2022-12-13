1ac6c3834492b6bb62ee353d550f14d0b45e3147
zakevicius <m.zakevicius@gmail.com>
2022-12-13 11:15:42 +0200

    not_to

---
+++ spec/26_not_to_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe 'not_to method' do
+  it 'checks for the inverse of a matcher' do
+    expect(5).not_to eq(10)
+    expect([1, 2, 3]).not_to equal([1, 2, 3])
+
+    expect(10).not_to be_odd
+    expect([1, 2, 3]).not_to be_empty
+
+    expect(nil).not_to be_truthy
+
+    expect('Canada').not_to start_with('car')
+    expect('Canada').not_to end_with('city')
+
+    expect(5).not_to respond_to(:length)
+
+    expect([1, 2, 3]).not_to include(4)
+
+    expect { 11 / 3 }.not_to raise_error
+  end
+end
