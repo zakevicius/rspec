fa34694b0b823f2e40f26425cfd4aa85da0bc2e0
zakevicius <m.zakevicius@gmail.com>
2022-12-13 10:52:21 +0200

    Respond_to matcher

---
+++ spec/24_respond_to_matcher_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class HotChocolate
+  def drink
+    'Delicious'
+  end
+
+  def discard
+    'PLOP!'
+  end
+
+  def purchase(number)
+    "Awesome, i just purchased #{number} beverages."
+  end
+end
+
+class Coffee
+  def drink; end
+
+  def discard; end
+
+  def purchase(number); end
+end
+
+RSpec.describe HotChocolate do
+  it 'confirms that an object can respond to a method' do
+    expect(subject).to respond_to(:drink)
+    expect(subject).to respond_to(:drink, :discard)
+    expect(subject).to respond_to(:drink, :discard, :purchase)
+  end
+
+  it 'confirms that an object can respond to a method with arguments' do
+    expect(subject).to respond_to(:purchase)
+    expect(subject).to respond_to(:purchase).with(1).arguments
+  end
+
+  it { is_expected.to respond_to(:purchase, :discard) }
+  it { is_expected.to respond_to(:purchase).with(1).arguments }
+end
