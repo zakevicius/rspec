b843266bd33ffbb702aee32019c1d392da751865
zakevicius <m.zakevicius@gmail.com>
2022-12-14 10:08:02 +0200

    Spies

---
+++ spec/33_spies_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+RSpec.describe 'spies' do
+  let(:animal) { spy('animal') }
+
+  it 'confirms that a message has been received' do
+    # without spies:
+    # expect(animal).to receive(:eat_food)
+    # animal.eat_food
+
+    animal.eat_food
+    expect(animal).to have_received(:eat_food)
+    expect(animal).not_to have_received(:eat_human)
+  end
+
+  it 'resets between examples' do
+    expect(animal).not_to have_received(:eat_food)
+  end
+
+  it 'retais same functionality of a regular double' do
+    animal.eat_food
+    animal.eat_food
+    animal.eat_food('Sushi')
+
+    expect(animal).to have_received(:eat_food).exactly(3).times
+    expect(animal).to have_received(:eat_food).at_least(2).times
+    expect(animal).to have_received(:eat_food).with('Sushi')
+    expect(animal).to have_received(:eat_food).with('Sushi').once
+  end
+end
